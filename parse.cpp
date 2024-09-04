dea851b8b5fcefdb5fece76ec5a4e40696e01ef5
Yash Chordia <yash.chordia00@gmail.com>
2024-09-05 00:56:22 +0530

    fixed parsing error

--- parse.cpp
+++ parse.cpp
@@ -14,42 +14,39 @@ vector<vector<char*>> parse(char* lines_read) {
         preParsedString.replace(pos, target.length(), "");
     }
 
-    string modifiableString = preParsedString;
-    char* modified_lines = new char[modifiableString.size() + 1];
-    strcpy(modified_lines, modifiableString.c_str());
-
-    char* tok;
     vector<char*> pre_commands;
+    stringstream ss(preParsedString);
+    string token;
 
-    char* saveptr;
-    tok = strtok_r(modified_lines, ";", &saveptr);
-    while (tok != NULL) {
-        pre_commands.push_back(strdup(tok));
-        tok = strtok_r(NULL, ";", &saveptr);
+    while (getline(ss, token, ';')) {
+        pre_commands.push_back(strdup(token.c_str()));
     }
     
     vector<char*> processed_commands;
-    string cmd;
     for (size_t i = 0; i < pre_commands.size(); ++i) {
-        cmd = (pre_commands[i]);i++;
-        while(count(cmd.begin(),cmd.end(),'\"') % 2!=0 && pre_commands[i]!=NULL){
-            cmd+=(";"+(string)pre_commands[i++]);
+        string cmd(pre_commands[i]);
+        free(pre_commands[i]);
+
+        // Handle commands with unmatched quotes
+        while (count(cmd.begin(), cmd.end(), '\"') % 2 != 0 && i + 1 < pre_commands.size()) {
+            cmd += ";" + string(pre_commands[++i]);
         }
+        
         processed_commands.push_back(strdup(cmd.c_str()));
-        cmd.clear();
     }
-    if(!cmd.empty())processed_commands.push_back(strdup(cmd.c_str()));
-    for(int i = 0 ; i < (int)pre_commands.size(); ++i) free(pre_commands[i]);
 
     vector<vector<char*>> intermediate;
     for (auto cmd : processed_commands) {
         vector<char*> tokens;
         char* sub_tok;
         char* sub_saveptr;
+        
         sub_tok = strtok_r(cmd, "\"", &sub_saveptr);
         while (sub_tok != NULL) {
-            string arg = (sub_tok);
-            if(arg[0] == '~')arg.replace(0,1,shellEnv.home_dir);
+            string arg(sub_tok);
+            if (arg[0] == '~') {
+                arg.replace(0, 1, shellEnv.home_dir);
+            }
             tokens.push_back(strdup(arg.c_str()));
             sub_tok = strtok_r(NULL, "\"", &sub_saveptr);
         }
@@ -76,7 +73,6 @@ vector<vector<char*>> parse(char* lines_read) {
         }
         parsedList.push_back(tokens);
     }
-    delete[] modified_lines;
 
     return parsedList;
 }
